d7f7a9f021a44b3482bb0ee8f4579f0fcddd8024
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-03-31 17:40:00 -0400

    output

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-# output "ec2-instance" {
-#   description = "IDs of the VPC's public subnets"
-#   value       = module.vpc.public_subnets
-# }
+output "ec2-instance" {
+  description = "IDs of the VPC's public subnets"
+  value       = module.ec2-instance.id
+}
