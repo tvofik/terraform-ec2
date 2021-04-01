3991508064a47854451e7fad178d9fec4bbb5e23
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-04-01 17:36:37 -0400

    region

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "ec2-instance" {
-  description = "IDs of the VPC's public subnets"
-  value       = module.ec2-instance.id
-}
+# output "ec2-instance" {
+#   description = "IDs of the VPC's public subnets"
+#   value       = module.ec2-instance.id
+# }
--- variable.tf
+++ variable.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  type = string
+  description = "Name of region to deploy to"
+  default = "us-east-1"
+}
+
 variable "name" {
   type = string
   description = "Name of instance"
